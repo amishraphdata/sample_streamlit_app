aa507570d5103de299b82f2518d663fcca6cf65e
amishraphdata <amishra@phdata.io>
2025-06-20 18:57:57 +0530

    Create 1_about.py

---
+++ OFYP6ZWYHMQZP969/utils/pages/1_about.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("About This App")
+st.write("""
+This is a sample multi-page Streamlit app for testing Git integration.
+- Add more pages in the `pages/` directory.
+- Edit, commit, and push to your Git repo to test your workflow!
+""")
